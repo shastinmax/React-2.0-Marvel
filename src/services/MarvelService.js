1025997d020587c02ee641cf52fc7c323d735b1a
shastinmax <shastin_maksim@mail.ru>
2021-11-19 15:34:08 +0300

    add RandomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,6 @@
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=d744a44799ab057e317cba56f8a2aed4'
     getResource = async (url) => {
         let res = await fetch(url)
 
@@ -7,8 +9,25 @@ class MarvelService {
         }
         return await res.json()
     }
-    getAllCharacters=()=>{
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=d744a44799ab057e317cba56f8a2aed4')
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    }
+
+    getCharacter =async (id) => {
+      const res =await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res)
+    }
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description: res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].url
+        }
     }
 }
+
 export default MarvelService
