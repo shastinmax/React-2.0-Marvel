27925c6181b7ef0be6c4048390217a221bc856c1
shastinmax <shastin_maksim@mail.ru>
2021-11-20 12:08:17 +0300

    refactor CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -5,7 +5,7 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
 
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
+
 import MarvelService from "../../services/MarvelService";
 
 
@@ -19,7 +19,7 @@ class CharInfo extends Component {
     componentDidMount() {
         this.updateChar()
     }
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProps) {
         if(this.props.charId !==prevProps.charId){
             this.updateChar()
         }
@@ -78,28 +78,26 @@ class CharInfo extends Component {
 }
 
 const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki} = char
+
     return (
         <>
             <div className="char__basics">
-                <img src={thor} alt="abyss"/>
+                <img src={thumbnail} alt={name}/>
                 <div>
-                    <div className="char__info-name">thor</div>
+                    <div className="char__info-name">{name}</div>
                     <div className="char__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
                 </div>
             </div>
             <div className="char__descr">
-                In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the
-                brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir,
-                and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the
-                stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse
-                Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.
+                {description}
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
