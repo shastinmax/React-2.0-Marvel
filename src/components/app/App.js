0e8adef3ceaebd0abbcaa58424b63e09dfb08654
shastinmax <shastin_maksim@mail.ru>
2021-11-26 10:33:37 +0300

    add component SingleComicPage.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import {MainPage, ComicsPage,Page404} from '../pages'
+import {MainPage, ComicsPage,Page404,SingleComicPage} from '../pages'
 import {Route, Routes} from 'react-router-dom'
 import AppHeader from "../appHeader/AppHeader";
 
@@ -12,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route exact path='/' element={<MainPage/>}/>
                     <Route exact path='/comics' element={<ComicsPage/>}/>
+                    <Route exact path='/comics/:comicId' element={<SingleComicPage/>}/>
                     <Route exact path='*' element={<Page404/>}/>
                 </Routes>
             </main>
